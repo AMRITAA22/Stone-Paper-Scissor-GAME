798399cb57f8da8d80e7104a47a9bb2409d66acb
AMRITAA22 <amritaa2202@gmail.com>
2025-08-19 09:49:04 +0530

    update script file

--- script.js
+++ script.js
@@ -34,22 +34,21 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   const playGame = (userChoice) => {
-    //Generate computer choice
     const compChoice = genCompChoice();
 
+    // highlight computer choice (minimal code)
+    document.querySelectorAll(".choice").forEach(c => c.classList.remove("highlight"));
+    document.getElementById(compChoice).classList.add("highlight");
+
     if (userChoice === compChoice) {
-      //Draw Game
       drawGame();
     } else {
       let userWin = true;
       if (userChoice === "rock") {
-        //scissors, paper
         userWin = compChoice === "paper" ? false : true;
       } else if (userChoice === "paper") {
-        //rock, scissors
         userWin = compChoice === "scissors" ? false : true;
       } else {
-        //rock, paper
         userWin = compChoice === "rock" ? false : true;
       }
       showWinner(userWin, userChoice, compChoice);
